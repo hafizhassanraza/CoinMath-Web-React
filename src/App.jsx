1f9fc45928c35a7625e0025a6c73b174794bbdf0
Hassan Raza <hafizhassanraza.hh@gmail.com>
2024-05-29 22:55:50 +0500

    Import

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ComplexNavbar } from './components/Header'
+import Main from './pages/Main'
+
+function App() {
+
+
+  return (
+    <>
+      <ComplexNavbar  />
+      <Main/>
+    </>
+  )
+}
+
+export default App
